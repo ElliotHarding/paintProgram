35188e802a6dedf1943d99fea583dd163446fb76
Elliot <elliot.one.harding@gmail.com>
2022-01-05 18:27:42 +0000

    wip - pan offset removed from all but canvas image

--- paintProgram/Canvas.cpp
+++ paintProgram/Canvas.cpp
@@ -47,12 +47,11 @@ void Canvas::updateCurrentTool(Tool t)
     if(m_tool != TOOL_DRAG)
     {
         std::lock_guard<std::mutex> lock(m_canvasMutex);
-        std::lock_guard<std::mutex> panLock(m_panOffsetMutex);
 
         //Dump dragged contents onto m_canvasImage
         QPainter painter(&m_canvasImage);
         painter.setCompositionMode (QPainter::CompositionMode_SourceOver);
-        painter.drawImage(QRect(m_dragOffsetX + m_panOffsetX * -1, m_dragOffsetY + m_panOffsetY * -1, m_clipboardImage.width(), m_clipboardImage.height()), m_clipboardImage);
+        painter.drawImage(QRect(m_dragOffsetX, m_dragOffsetY, m_clipboardImage.width(), m_clipboardImage.height()), m_clipboardImage);
 
         recordImageHistory();
 
@@ -137,12 +136,14 @@ void Canvas::pasteKeysPressed()
     m_selectedPixels.clear();
     m_selectionTool->setGeometry(QRect(m_selectionToolOrigin, QSize()));
 
+    std::lock_guard<std::mutex> panLock(m_panOffsetMutex);
+
     for(int x = 0; x < m_clipboardImage.width(); x++)
     {
         for(int y = 0; y < m_clipboardImage.height(); y++)
         {
             if(m_clipboardImage.pixelColor(x,y).alpha() > 0)
-                m_selectedPixels.push_back(QPoint(x,y));
+                m_selectedPixels.push_back(QPoint(x, y));
         }
     }
 
@@ -218,7 +219,7 @@ void Canvas::paintEvent(QPaintEvent *paintEvent)
     for(QPoint p : m_selectedPixels)
     {
         //TODO ~ If highlight selection color and background color are the same we wont see highlighted area...
-        painter.fillRect(QRect(p.x() + m_panOffsetX, p.y() + m_panOffsetY, 1, 1), m_c_selectionAreaColor);
+        painter.fillRect(QRect(p.x(), p.y(), 1, 1), m_c_selectionAreaColor);
     }
 
     //Draw selection tool
@@ -569,7 +570,6 @@ void Canvas::floodFillOnSimilar(QImage &image, QColor newColor, int startX, int 
 void Canvas::prepClipBoard()
 {
     std::lock_guard<std::mutex> lock(m_canvasMutex);
-    std::lock_guard<std::mutex> panLock(m_panOffsetMutex);
 
     //Prep selected pixels for dragging
     m_clipboardImage = QImage(QSize(m_canvasImage.width(), m_canvasImage.height()), QImage::Format_ARGB32);
@@ -580,7 +580,7 @@ void Canvas::prepClipBoard()
     dragPainter.setCompositionMode (QPainter::CompositionMode_Source);
     for(QPoint p : m_selectedPixels)
     {
-        dragPainter.fillRect(QRect(p.x() + m_panOffsetX, p.y() + m_panOffsetY, 1, 1), m_canvasImage.pixelColor(p.x(), p.y()));
+        dragPainter.fillRect(QRect(p.x(), p.y(), 1, 1), m_canvasImage.pixelColor(p.x(), p.y()));
     }
 }
 
